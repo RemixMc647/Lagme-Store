1f1cd101f8a1b8cc2c4f2e72e35f1caff6ac96a8
Adelabu Aderemi Rasheed <adelabuaderemi02@gmail.com>
2026-09-10 19:07:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
@@ -5,6 +6,17 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'www')));
+
+app.get('/verify-payment', async (req, res) => {
+  const reference = req.query.reference;
+  const response = await fetch(`https://api.paystack.co/transaction/verify/${reference}`, {
+    headers: {
+      Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`
+    }
+  });
+  const data = await response.json();
+  res.json(data.data);
+});
 
 app.get('/*splat', (req, res) => {
   res.sendFile(path.join(__dirname, 'www', 'index.html'));
